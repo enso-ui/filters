4622e489d6f19dbfef6b2f9e3f45715b21e22e24
mishu <mishu@xtelecom.ro>
2019-09-20 15:59:52 +0300

    adds filter state

--- renderless/index.js
+++ renderless/index.js
@@ -1,5 +1,8 @@
 import CoreIntervalFilter from '../src/renderless/CoreIntervalFilter.vue';
 import CoreDateFilter from '../src/renderless/CoreDateFilter.vue';
 import CoreDateIntervalFilter from '../src/renderless/CoreDateIntervalFilter.vue';
+import FilterState from '../src/renderless/FilterState.vue';
 
-export { CoreIntervalFilter, CoreDateFilter, CoreDateIntervalFilter };
+export {
+    CoreIntervalFilter, CoreDateFilter, CoreDateIntervalFilter, FilterState,
+};
