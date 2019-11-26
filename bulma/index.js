f693839a41eee5d38fa74516d162486db1a45048
reza <raftx24@gmail.com>
2019-11-26 13:51:11 +0330

    adds input filter

--- bulma/index.js
+++ bulma/index.js
@@ -1,17 +1,19 @@
 import IntervalFilter from '../src/bulma/IntervalFilter.vue';
+import InputFilter from '../src/bulma/InputFilter.vue';
 import DateFilter from '../src/bulma/DateFilter.vue';
 import DateIntervalFilter from '../src/bulma/DateIntervalFilter.vue';
 import VueFilter from '../src/bulma/VueFilter.vue';
 import SelectFilter from '../src/bulma/SelectFilter.vue';
 import BooleanFilter from '../src/bulma/BooleanFilter.vue';
 import EnsoIntervalFilter from '../src/bulma/EnsoIntervalFilter.vue';
+import EnsoInputFilter from '../src/bulma/EnsoInputFilter.vue';
 import EnsoDateFilter from '../src/bulma/EnsoDateFilter.vue';
 import EnsoDateIntervalFilter from '../src/bulma/EnsoDateIntervalFilter.vue';
 import EnsoFilter from '../src/bulma/EnsoFilter.vue';
 import EnsoSelectFilter from '../src/bulma/EnsoSelectFilter.vue';
 
 export {
-    IntervalFilter, EnsoIntervalFilter, DateFilter, EnsoDateFilter,
+    InputFilter, IntervalFilter, EnsoIntervalFilter, DateFilter, EnsoDateFilter,
     DateIntervalFilter, EnsoDateIntervalFilter, BooleanFilter,
-    VueFilter, EnsoFilter, SelectFilter, EnsoSelectFilter,
+    VueFilter, EnsoFilter, SelectFilter, EnsoSelectFilter, EnsoInputFilter
 };
---
+++ src/renderless/CoreInputFilter.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'CoreInputFilter',
+
+    props: {
+        value: {
+            type: String,
+            default: 'text',
+        },
+        type: {
+            type: String,
+            default: 'text',
+        },
+    },
+
+    render() {
+        return this.$scopedSlots.default({
+            value: this.value,
+            bindings: {
+                value: this.value,
+                type: this.type,
+            },
+            events: {
+                input: (e) => {
+                    this.$emit('input', e.target.value);
+                },
+            },
+            clearEvents: {
+                click: () => {
+                    this.$emit('input', null);
+                },
+            },
+        });
+    },
+};
+</script>
